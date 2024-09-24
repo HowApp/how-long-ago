3ed602bf5c6f1395165f6af7dedfa3cccda25e2f
Mykhailo <rosmisha@gmail.com>
2024-09-24 20:42:53 +0300

    fix

--- Core/Infrastructure/Hubs/FileProcessingHub.cs
+++ Core/Infrastructure/Hubs/FileProcessingHub.cs
@@ -7,15 +7,29 @@ using Microsoft.Extensions.Logging;
 public class FileProcessingHub : Hub
 {
     private readonly ILogger<FileProcessingHub> _logger;
+
+    public FileProcessingHub(ILogger<FileProcessingHub> logger)
+    {
+        _logger = logger;
+    }
+
     public override async Task OnConnectedAsync()
     {
         try
         {
             var userId = Context.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            await Groups.AddToGroupAsync(Context.ConnectionId, userId);
-        
-            Console.WriteLine($"User {userId} is connected");
-        
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+                Console.WriteLine($"User {userId} was connected");
+            }
+            else
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, "unauthorized");
+                Console.WriteLine($"Unauthorized User was connected");
+            }
+
             await base.OnConnectedAsync();
         }
         catch (Exception e)
